4fbc789dc84000967a4fa9f8264baa98ec9ee613
gnilvan <gnilvan@users.noreply.github.com>
2023-04-02 16:56:04 +0200

    second test added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function greet(namePassedIn) {
   return `Welcome to SALT, ${namePassedIn}`;
 }
-
 const greeting = greet( 'Marcus' );
 if(greeting === 'Welcome to SALT, Marcus') {
+  console.log('IT WORKS');
+} else {
+  console.log('IT FAILS');
+}
+
+const secondGreeting = greet( 'Eliza' );
+if(secondGreeting === 'Welcome to SALT, Eliza') {
   console.log('IT WORKS');
 } else {
   console.log('IT FAILS');
--- test.js
+++ test.js
@@ -1,9 +1,30 @@
-const assert = require("assert");
+const { greet } = require('./index.js');
 
-describe("great function", () => {
-  it("should greet Marcus properly", () => {
-    const greeter = require('./index.js');
-    const actualResult = greeter.greet('Marcus');
-    assert.equal('Welcome to SALT, Marcus', actualResult);
+describe ('greeting functions', () => {
+  test('Marcus', () => {
+    expect(greet('Marcus')).toEqual('Welcome to SALT, Marcus');
+  });
+  test('Eliza', () => {
+    expect(greet('Eliza')).toEqual('Welcome to SALT, Eliza');
   });
 });
+
+
+
+
+
+
+// const assert = require("assert");
+
+// describe("great function", () => {
+//   it("should greet Marcus properly", () => {
+//     const greeter = require('./index.js');
+//     const actualResult = greeter.greet('Marcus');
+//     assert.equal('Welcome to SALT, Marcus', actualResult);
+//   });
+// });
+
+// test('should greet Eliza properly', () => {
+//   const greet = 'Welcome to SALT, Eliza';
+//   expect(greet(greet)).toEqual(greet)
+// });
